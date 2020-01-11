43e900eb886af9902ab827d60eb3e8d82600e445
manasa-cm27 <manasa7n93@gmail.com>
2020-01-11 19:16:59 +0530

    3rd level changes

--- admission.php
+++ admission.php
@@ -1,11 +1,8 @@
 <!-- Header Section -->
-<?php  include('header.php')   ?>
-
-</ul>
-</div>
+<?php  include('header.php'); ?>
 <!-- /.navbar-collapse -->
 </nav>
-</div>   
+</div>
 </div>
 <!-- Page Title Section -->
 <div class="row page-title page-title-about">
@@ -13,24 +10,35 @@
 <h2>Admission Process</h2>
 </div>
 </div>
+
 <!-- Teachers row -->
 <div class="row section-row">
 <div class="container">
-<div class="section-row-header-center">
-<h6>
-<i class="fa fa-star-o"></i>WE ARE BEST
-<i class="fa fa-star-o"></i>
-</h6>
-  
-</div>
-<div class="teacher-item col-sm-12 teacher-about-row">
-<h1>Admissions for the Academic Year 2019-20 have begun. </h1>
-<p>Parents seeking admission to Jain Public School are requested to collect the admission form from our centers:  </p>
-<p>The completed application form with all the required documents mentioned in the application form should be submitted at the centers from Monday to Saturday 9.00 am to 4.00pm </p>
-</div>
-</div>
-</div>
+<h3>Application Form</h3>
+<p1>The brochure and application form can be obtained from the school office by paying Rs.250.</p1>
+<h3>Submission of Application Form</h3>
+<p2>Duly filled in application form can be submitted to us through the following process.</p2>
+        <p><li>In person at our school office.</li></p>  
+        <p><li>via registered post / courier.</li></p>
+<p2>Application form should accompany the following copy of documents( as applicable)<p2>
+<p style="text-align:justify;"><li>Birth Certificate </li></p> 
+<p style="text-align:justify;"><li>Transfer Certificate / school records for the past 2 years</li></p> 
+<p style="text-align:justify;"><li>Adhar Card of student</li></p> 
+<p style="text-align:justify;"><li>4 Passport size photo of student </li></p> 
+<p style="text-align:justify;"><li>Proof of address </li></p> 
+<p style="text-align:justify;"><li>Aadhar Card of Parent's</li></p> 
+<p style="text-align:justify;"><li>2 Passport size photo of parent's </li></p> 
+<h3>Entrance Test</h3>
+<p1>The student will have to appear for the “Admission Test” which may be written or oral as laid down by the School for different classes. Subjects like English, Hindi, Mathematics and/or any other subject which may be considered necessary will be listed.</p1>
+<h3>Interview </h3>
+<p1>An interview with the Principal / Head of the Institution.</p>
+<h3>Results</h3>
+<p2>Please contact the Manager or in-charge of admission for further assistance by email or phone to schedule an appointment.</p2>
+<p>Ph:8861814011</p>
 
 
 <!-- Footer Section -->
+</div>
+</div>
+
 <?php  include('footer.php')   ?>
